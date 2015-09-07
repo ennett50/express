01b066facc30061c2390c7867baa2efd76b0dcbc
ennet <ennet@mail.ru>
2015-09-07 10:20:26 +0300

    edit config db

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.set('debug', true);
 
-mongoose.connect('mongodb://localhost/documentation', {
+mongoose.connect('mongodb://admin:qwerty@localhost/documentation', {
     server: {
         poolSize: 5
     }
